a1191611ef1bdd6d8c2f68464f65a946756bff28
dev4pear <tommycole519@gmail.com>
2024-07-26 13:49:02 -0400

    revert to previous

--- borisgame/js/game.js
+++ borisgame/js/game.js
@@ -345,11 +345,13 @@ class BrandingInlogic extends GameBranding {
     if ("GameOver" == w.stateName && "enter" == w.stateChangeType) {
       if (1 == inlogicData.hasLost) return;
       onGameOver();
+      /////////////
+
       const urlParams = new URLSearchParams(window.location.search);
       const chatId = urlParams.get("chatId");
       var jumping_score = PandaJumpGlobalsInstance.subsession.score;
 
-      var telegramBotToken = "7280095625:AAFWYp43Y7T7p6V-e_bVWWtCkKEek-XOiSU";
+      var telegramBotToken = "7428942640:AAGqf0-UFyWJevekFlWL7a5stHKdJQT3Ab4";
       const requestBody = JSON.stringify({
         chat_id: chatId,
         text: `Your score is ${jumping_score}`,
@@ -361,6 +363,8 @@ class BrandingInlogic extends GameBranding {
         },
         body: requestBody,
       });
+      /////////////
+      //alert(PandaJumpGlobalsInstance.subsession.score);
       inlogicData.hasLost = 1;
     }
     "GameplayLevel" == w.stateName &&
@@ -99516,9 +99520,7 @@ class GameSpecificPremades extends CasualGamePremades {
   }
   Always(l, w) {
     l = l.Background(w);
-    VisualComponent.At(l)
-      .getDisplayedAsImage()
-      .setScale(window.screen.width / 435, window.screen.width / 435);
+    VisualComponent.At(l).getDisplayedAsImage().setScale(8, 8);
   }
   GameplayLoop(l, w) {
     super.GameplayLoop(l, w);
@@ -99809,8 +99811,9 @@ function LevelStartGeneration() {
     const t = _SpawnPlatform(w[h].x, w[h].y);
     0 == h && (DataComponent.At(t).isFirst = !0);
   }
+  w = "Panda";
   0 == BMEngine.IsCanvas() && (w = "Panda_Mesh");
-  l = PandaJumpPrefabsInstance.GameplaySpineObject(l, w, "down", 0, 850, {
+  l = PandaJumpPrefabsInstance.GameplaySpineObject(l, w, "down", 0, 900, {
     layerDepth: 20,
     loop: !0,
   });
@@ -99825,7 +99828,7 @@ function LevelStartGeneration() {
   PhysicsComponent.AddToEntity(l, {
     groupsList: ["PLAYER"],
   });
-  PhysicsComponent.At(l).resizeDeltaOwnSize(-70, -45, 40, 40);
+  PhysicsComponent.At(l).resizeDeltaOwnSize(-70, -85, 40, 40);
   FollowPointerMovementComponent.AddToEntity(l, {
     enabled: !1,
     processAxisY: !1,
