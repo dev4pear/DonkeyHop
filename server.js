4a4a9aa0bc58fff4d85d0e6ede5bc93257b59876
dev4pear <tommycole519@gmail.com>
2024-07-31 10:57:46 -0400

    update port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
 const telegramApiUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
 
